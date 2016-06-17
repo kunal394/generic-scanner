8bc304185fabc7258483ce8d0ec9ba8bd14a5216
Kunal Singh <knlsingh394@gmail.com>
2016-06-17 18:17:02 +0530

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -38,12 +38,12 @@ if [ "$#" -eq 0 ]; then
     echo "	nik => Nikto"
     echo "	zap => OWASP ZAP"
     echo "	nmap => Nmap"
-    echo "  fish => Skipfish" 
-    echo "  w3af => W3AF" 
-    echo "  bash => Spawn a shell"
-    echo "  pytbull => PYTBULL"
-    echo "  sqlmap => Sqlmap"
-    echo "  wapiti => Sqlmap"
+    echo "  	fish => Skipfish" 
+    echo "  	w3af => W3AF" 
+    echo "  	bash => Spawn a shell"
+    echo "  	pytbull => PYTBULL"
+    echo "  	sqlmap => Sqlmap"
+    echo "  	wapiti => Sqlmap"
     echo "	all => Run all scanners"
     exit 1
 fi
@@ -60,34 +60,36 @@ case $scanner_id in
 	nmap) 
 	nmap "$@"
 	;;
-    fish)
-    cd $FISH_HOME/skipfish-2.10b
+    	fish)
+    	cd $FISH_HOME/skipfish-2.10b
 	./skipfish -o ./out "$@"
 	;;
 	w3af) 
-	$W3AF_HOME/w3af_console "$@"
+	echo "under construction"
+	#$W3AF_HOME/w3af_console "$@"
 	;;
 	pytbull) 
-	$BULL_HOME/pytbull "$@"
+	echo "under construction"
+	#$BULL_HOME/pytbull "$@"
 	;;
 	sqlmap) 
 	$SQLMAP_HOME/sqlmap.py "$@"
 	;;
 	wapiti) 
-    $WAPITI_HOME/bin/wapiti "$@"
+    	$WAPITI_HOME/bin/wapiti "$@"
 	;;
 	bash) 
 	/bin/bash
 	;;
-    all) 
+    	all) 
 	perl $NIKTO_HOME/nikto.pl "$@"
-    bash $ZAP_HOME/zap.sh "$@"
-    nmap "$@"
-    cd $FISH_HOME/skipfish-2.10b && ./skipfish -o ./out "$@"
-    #$W3AF_HOME/w3af_console "$@"
+    	bash $ZAP_HOME/zap.sh "$@"
+    	nmap "$@"
+    	cd $FISH_HOME/skipfish-2.10b && ./skipfish -o ./out "$@"
+    	#$W3AF_HOME/w3af_console "$@"
 	#$BULL_HOME/pytbull "$@"
-    $SQLMAP_HOME/sqlmap.py "$@"
-    $WAPITI_HOME/bin/wapiti "$@"
+    	$SQLMAP_HOME/sqlmap.py "$@"
+    	$WAPITI_HOME/bin/wapiti "$@"
 	;;
 	*) 
 	echo "Unknow Scanner ID !"
